87bca1193eb259aba634ac0127ae5336df810931
Dept1k <93694988+Dept1k@users.noreply.github.com>
2022-01-15 02:10:49 +0700

    burger

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+const menuLinks = document.querySelectorAll(".nav__link[data-goto]");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        scrollY -
+        document.querySelector("header").offsetHeight;
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      document.querySelector(".header__menu").classList.toggle("nav__visible");
+      e.preventDefault();
+    }
+  }
+}
+
+const menuButton = document.querySelector(".nav__button");
+menuButton.addEventListener("click", function () {
+  document.querySelector(".header__menu").classList.toggle("nav__visible");
+});
